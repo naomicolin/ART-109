07d7935d7cd268fa5624d304868097007585906a
NAOMI <naomi.colin@yahoo.com>
2024-03-12 00:01:08 -0700

    /////

---
+++ CLASS TUTORIALS/THE GREEN MONSTER/main.js
@@ -0,0 +1,86 @@
+document.querySelector("#monster1").addEventListener("click", function(){
+    document.querySelector("#monster1").style.visibility = "hidden";
+})
+
+document.querySelector("#monster2").addEventListener("click", function(){
+    document.querySelector("#monster2").style.visibility = "hidden";
+})
+
+document.querySelector("#monster3").addEventListener("click", function(){
+    document.querySelector("#monster3").style.visibility = "hidden";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log ("hello hello");
+
+// let pageTitle = document.querySelector("#page-title");
+// //// instead of using the whole code below commented out belowwwww
+
+
+
+
+
+
+
+// ////JAVASCRIPT TIMEOUT CHANGES H1 TITLE AFTER 3 SECONDS
+// setTimeout(function(){
+//     document.querySelector("#page-title").style.color = "pink";
+//     console.log("timeout worked!"); 
+// }, 3000);
+
+
+
+
+
+// ///WHEN HEADER IS CLICKED, CHANGES BG COLOR
+// document.querySelector("header").onclick = function() { 
+//     console.log("clciked on header");
+//     document.querySelector("body").style.backgroundColor = "yellow";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // OTHER CODE
+
+// // ////JAVASCRIPT TIMEOUT CHANGES H1 TITLE AFTER 3 SECONDS
+// // setTimeout(function(){
+// //     document.querySelector("#page-title").style.color = "pink";
+// //     console.log("timeout worked!"); 
+// // }, 3000)
+
+
+// // ///WHEN HEADER IS CLICKED, CHANGES BG COLOR
+// // document.querySelector("header").onclick = function() { 
+// //     console.log("clciked on header");
+// //     document.querySelector("body").style.backgroundColor = "yellow";
+// // }
+
