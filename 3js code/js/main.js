db502f536f440d2d6089914d0611b943abab53d1
NAOMI <naomi.colin@yahoo.com>
2024-03-25 19:47:40 -0700

    //

--- 3js code/js/main.js
+++ 3js code/js/main.js
@@ -7,7 +7,7 @@ import * as THREE from 'three';
 
 // Import add-ons
 import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js';
-// import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders/GLTFLoader.js'; // to load 3d models
+import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders/GLTFLoader.js'; // to load 3d models
 
 
 
@@ -15,15 +15,39 @@ import { OrbitControls } from 'https://unpkg.com/three@0.162.0/examples/jsm/cont
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+
+
+const renderer = new THREE.WebGLRenderer({antialias});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial( {color: 0x0000ff});
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube ); 
+
+camera.position.z = 5;
+
+
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.02;
+    cube.rotation.y += 0.02;
+
+
+    renderer.render(scene, camera);
+
+
+}
+
+animate();
 
 
 // ~~~~~~~~~~~~~~~~ Initiate add-ons ~~~~~~~~~~~~~~~~
 const controls = new OrbitControls(camera, renderer.domElement);
-// const loader = new GLTFLoader(); // to load 3d models
+const loader = new GLTFLoader(); // to load 3d models
 
 
 // →→→→→→ Follow next steps in tutorial: // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
